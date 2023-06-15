2cdf432e24aaaeeb4f3e041e9c5bb739a8e4e8af
Enio Schaefer <enio100schaefer@hotmail.com>
2023-06-14 22:27:07 -0300

    feat: req 2 concluido

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        with open(path_file, "r") as file:
+            if '.txt' not in path_file:
+                print("Formato inválido", file=sys.stderr)
+            else:
+                return file.read().splitlines()
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+
+print(txt_importer('statics/arquivo_teste.txt'))
