a4f05bc9c416324efe6afa112e5105f890f8b214
Enio Schaefer <enio100schaefer@hotmail.com>
2023-06-14 21:49:40 -0300

    feat: req 1 concluido

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,24 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
+        self.__length = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.__length
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
+        self.__length += 1
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.__length == 0:
+            return None
+        self.__length -= 1
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index > self.__length - 1:
+            raise IndexError("Índice Inválido ou Inexistente")
+        else:
+            return self._data[index]
